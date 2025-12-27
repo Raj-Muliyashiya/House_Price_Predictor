1a71c7fe99acaaafe7f166c288704e0517140257
RajxMer <rajxmer@gmail.com>
2025-12-27 10:03:55 +0530

    complete app_ui 1.1

--- app_ui.py
+++ app_ui.py
@@ -4,6 +4,8 @@ import requests
 
 st.title("Bangalore House Price Predictor")
 
+st.divider()    
+
 base_url = requests.get('http://localhost:8000/get_locations')
 location_request = base_url.json()
 location_list = (location_request['location'])
@@ -13,12 +15,12 @@ main_location = st.selectbox("Location" ,location_list,index=None, placeholder="
 col1, col2 = st.columns(2)
 
 with col1:
-    bhk = st.number_input("BHK", min_value=1 , max_value=10)
+    bhk = st.number_input("BHK", min_value=1 , max_value=8)
 
 with col2:
-    bath = st.number_input("Bath", min_value=1 , max_value=10)
+    bath = st.number_input("Bath", min_value=1 , max_value=8)
 
-sqrft = st.slider("Total Area (sqft)", min_value=300 , max_value=10000)
+sqrft = st.slider("Total Area (sqft)", min_value=600 , max_value=5000)
 
 
 if st.button("Predict Price"):
@@ -31,9 +33,15 @@ if st.button("Predict Price"):
             'total_sqft' : sqrft,
             'bath' : bath
         }
+        with st.spinner():
+            json_response = requests.post('http://localhost:8000/predict_home_price', json=inpud_data)
+            response = json_response.json()["the prediction is "]
 
-        response = requests.post('http://localhost:8000/predict_home_price', json=inpud_data)
-        response = response.json()
+            final_price = max(10.0, response)
         
-        st.metric(label="Estimated Property Price" ,value=f"₹ {response["the prediction is "]:.2f} Lakhs")
+        st.metric(label="Estimated Property Price" ,value=f"₹ {final_price:.2f} Lakhs")
+
+        chart_data = {"Your House": final_price, "Average": 90}
+        st.bar_chart(chart_data, horizontal=True)
+
         st.balloons()
