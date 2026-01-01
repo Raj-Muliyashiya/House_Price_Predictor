24ad85025914710c466a7c9c1365add9d4d1f672
RajxMer <rajxmer@gmail.com>
2026-01-01 18:45:15 +0530

    add features

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 import streamlit as st
 import requests
 
+st.set_page_config(
+    page_title="House Price Predictor",
+    page_icon="🏡",               
+)
 
 st.title("Bangalore House Price Predictor")
 
 st.divider()    
 
-base_url = requests.get('https://house-price-predictor-2ws5.onrender.com/get_locations')
-location_request = base_url.json()
-location_list = (location_request['location'])
+@st.cache_data
+def get_location_list():
+    base_url = requests.get('https://house-price-predictor-2ws5.onrender.com/get_locations')
+    return base_url.json()['location']
 
-main_location = st.selectbox("Location" ,location_list,index=None, placeholder="Search for a location...")
+try:
+    location_list = get_location_list()
+
+except:
+    st.error("Server is asleep. Please wait a moment and reload.")
+    location_list = []
+
+main_location = st.selectbox("Location" ,get_location_list(),index=None, placeholder="Search for a location...")
 
 col1, col2 = st.columns(2)
 
@@ -27,14 +39,14 @@ if st.button("Predict Price"):
     if main_location is None:
         st.error("please select the location")
     else:
-        inpud_data ={
+        input_data ={
             'location' : main_location,
             'bhk' : bhk,
             'total_sqft' : sqrft,
             'bath' : bath
         }
         with st.spinner():
-            json_response = requests.post('https://house-price-predictor-2ws5.onrender.com/predict_home_price', json=inpud_data)
+            json_response = requests.post('https://house-price-predictor-2ws5.onrender.com/predict_home_price', json=input_data)
             response = json_response.json()["the prediction is "]
 
             final_price = max(10.0, response)
@@ -43,5 +55,3 @@ if st.button("Predict Price"):
 
         chart_data = {"Your House": final_price, "Average": 90}
         st.bar_chart(chart_data, horizontal=True)
-
-        st.balloons()
